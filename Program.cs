4f6eef547ca32ddf131ed032d2c5866794135bee
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-12-27 11:17:17 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("##Nulable Types ##\n");
+
+
+int? x = 20;
+int? y = 40;
+int? z = x * y;
+
+Console.WriteLine(z);
+Console.ReadLine();
+
+int? b = null;
+if (b.HasValue)
+{
+Console.WriteLine($"b = {b.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor");
+}
+
+    
